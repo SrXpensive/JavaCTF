84c6d61d29af1b43dffd5781098d5de969e9a54c
Edu Caro <eduardo.caro.lorente@gmail.com>
2024-05-27 08:15:38 +0200

    Atributos de las clases Participante y Equipo

---
+++ src/Participante.java
@@ -0,0 +1,7 @@
+public abstract class Participante {
+    private String nombre;
+    private int puntosGanados;
+    private int[] flags = new int [3];
+    private int nIntentos;
+    private Equipo equipo;
+}
