4e212f752e7fd96576bef50cff7a0ebde11e3088
Edu Caro <eduardo.caro.lorente@gmail.com>
2024-05-27 08:32:15 +0200

    Constructor clase Participante

--- src/Participante.java
+++ src/Participante.java
@@ -4,4 +4,14 @@ public abstract class Participante {
     private int[] flags = new int [3];
     private int nIntentos;
     private Equipo equipo;
+
+    public Participante(String nombre, int puntosGanados, int[] flags, int nIntentos, Equipo equipo) {
+        this.nombre = nombre;
+        this.puntosGanados = puntosGanados;
+        this.flags = flags;
+        this.nIntentos = nIntentos;
+        this.equipo = equipo;
+    }
+
+    ;
 }
